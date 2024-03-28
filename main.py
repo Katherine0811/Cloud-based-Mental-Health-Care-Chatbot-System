f74c2912845807915e3f52504e82cb2fa9a7df88
Katherine0811 <katherine.lee0356@gmail.com>
2024-03-28 14:39:38 +0000

    Merge Login Pages and Chatbot Pages

--- main.py
+++ main.py
@@ -2,144 +2,215 @@ from tkinter import *
 import tkinter.messagebox as tkMessageBox
 import sqlite3
 
-root = Tk()
-root.title("Python: Simple Inventory System")
- 
-width = 640
-height = 480
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
-x = (screen_width/2) - (width/2)
-y = (screen_height/2) - (height/2)
-root.geometry("%dx%d+%d+%d" % (width, height, x, y))
-root.resizable(0, 0)
-
-
-#=======================================VARIABLES=====================================
-USERNAME = StringVar()
-PASSWORD = StringVar()
-FIRSTNAME = StringVar()
-LASTNAME = StringVar()
+from training import *
 
 #=======================================METHODS=======================================
-def Database():
-    global conn, cursor
+def database():
     conn = sqlite3.connect("db_member.db")
     cursor = conn.cursor()
     cursor.execute("CREATE TABLE IF NOT EXISTS `member` (mem_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username TEXT, password TEXT, firstname TEXT, lastname TEXT)")
+    cursor.close()
+    conn.close()
+
+def login_form(panel):
+    # Create Login Window
+    panel.withdraw()
+    loginFrame = Toplevel()
+    loginFrame.title("Login Portal")
+    loginFrame.geometry('1000x500')
+    loginFrame.configure(background = '#DBD0C0')
+    
+    # Display Labels
+    lbl_username = Label(loginFrame, text = "Username:", bg = '#DBD0C0', font = ('times', 15, 'bold'))
+    lbl_username.place(x = 300, y = 200)
+    lbl_password = Label(loginFrame, text = "Password:", bg = '#DBD0C0', font = ('times', 15, 'bold'))
+    lbl_password.place(x = 300, y = 250)
+
+    # User Input
+    username = Entry(loginFrame, bg = '#DBD0C0', font = ('times', 15, 'bold'), width = 40)
+    username.place(x = 450, y = 200)
+    password = Entry(loginFrame, bg = '#DBD0C0', font = ('times', 15, 'bold'), width = 40, show = "*")
+    password.place(x = 450, y = 250)
+
+    # Display Buttons
+    btn_login = Button(loginFrame, command = lambda: login(loginFrame, username, password), text = "Login", bg = '#FAEEE0', width = 20, height = 2, borderwidth = 3,
+                       relief = "ridge", activebackground = '#F9E4C8', font = ('times', 15, 'bold'))
+    btn_login.place(x = 300, y = 350)
+    btn_register = Button(loginFrame, command = lambda: register_form(loginFrame), text = "Register", bg = '#FAEEE0', width = 20, height = 2, borderwidth = 3,
+                       relief = "ridge", activebackground = '#F9E4C8', font = ('times', 15, 'bold'))
+    btn_register.place(x = 600, y = 350)
+    
+    # Exit Protocol
+    loginFrame.wm_protocol("WM_DELETE_WINDOW", lambda: exit(loginFrame, root))
+
+def register_form(panel):
+    # Create Register Window
+    panel.withdraw()
+    registerFrame = Toplevel()
+    registerFrame.title("Register Portal")
+    registerFrame.geometry('1000x500')
+    registerFrame.configure(background = '#DBD0C0')
+
+    # Display Labels
+    lbl_username = Label(registerFrame, text = "Username:", bg = '#DBD0C0', font = ('times', 15, 'bold'))
+    lbl_username.place(x = 300, y = 200)
+    lbl_password = Label(registerFrame, text = "Password:", bg = '#DBD0C0', font = ('times', 15, 'bold'))
+    lbl_password.place(x = 300, y = 250)
+    lbl_firstname = Label(registerFrame, text = "Firstname:", bg = '#DBD0C0', font = ('times', 15, 'bold'))
+    lbl_firstname.place(x = 300, y = 300)
+    lbl_lastname = Label(registerFrame, text = "Lastname:", bg = '#DBD0C0', font = ('times', 15, 'bold'))
+    lbl_lastname.place(x = 300, y = 350)
+
+    # User Input
+    username = Entry(registerFrame, bg = '#DBD0C0', font = ('times', 15, 'bold'), width = 40)
+    username.place(x = 450, y = 200)
+    password = Entry(registerFrame, bg = '#DBD0C0', font = ('times', 15, 'bold'), width = 40, show = "*")
+    password.place(x = 450, y = 250)
+    firstname = Entry(registerFrame, bg = '#DBD0C0', font = ('times', 15, 'bold'), width = 40)
+    firstname.place(x = 450, y = 300)
+    lastname = Entry(registerFrame, bg = '#DBD0C0', font = ('times', 15, 'bold'), width = 40)
+    lastname.place(x = 450, y = 350)
+
+    # Display Buttons
+    btn_login = Button(registerFrame, command = lambda: register(username, password, firstname, lastname), text = "Register", bg = '#FAEEE0', width = 20, height = 2, borderwidth = 3,
+                       relief = "ridge", activebackground = '#F9E4C8', font = ('times', 15, 'bold'))
+    btn_login.place(x = 300, y = 400)
+    lbl_register = Button(registerFrame, command = lambda: login_form(registerFrame), text = "Login", bg = '#FAEEE0', width = 20, height = 2, borderwidth = 3,
+                       relief = "ridge", activebackground = '#F9E4C8', font = ('times', 15, 'bold'))
+    lbl_register.place(x = 600, y = 400)
+    
+    # Exit Protocol
+    registerFrame.wm_protocol("WM_DELETE_WINDOW", lambda: exit(registerFrame, root))
+
+def register(username, password, firstname, lastname):
+    conn = sqlite3.connect("db_member.db")
+    cursor = conn.cursor()
+
+    # Retrieve value from user input
+    username = username.get()
+    password = password.get()
+    firstname = firstname.get()
+    lastname = lastname.get()
+
+    # User input validation
+    if username == "" or password == "" or firstname == "" or lastname == "":
+        warn = "Please complete the required field!"
+        tkMessageBox.showerror('', warn)
+    else:
+        cursor.execute("SELECT * FROM `member` WHERE `username` = ?", [username])
+        if cursor.fetchone() is not None:
+            warn = "Username is already taken"
+            tkMessageBox.showerror('', warn)
+        else:
+            cursor.execute("INSERT INTO `member` (username, password, firstname, lastname) VALUES(?, ?, ?, ?)", [username, password, firstname, lastname])
+            conn.commit()
+            tkMessageBox.showinfo('Account Status', 'Successfully Created!')
+    cursor.close()
+    conn.close()
+
+def login(panel, username, password):
+    conn = sqlite3.connect("db_member.db")
+    cursor = conn.cursor()
+
+    # Retrieve value from user input
+    username = username.get()
+    password = password.get()
+
+    # User input validation
+    if username == "":
+        warn = "Username can't be empty!"
+        tkMessageBox.showerror('', warn)
+    elif password == "":
+        warn = "Password can't be empty!"
+        tkMessageBox.showerror('', warn)
+    else:
+        cursor.execute("SELECT * FROM `member` WHERE `username` = ? and `password` = ?", [username, password])
+        if cursor.fetchone() is not None:         
+            cursor.close()
+            conn.close()
+            tkMessageBox.showinfo('Login Status', 'Logged in Successfully!')
+            chat_bot(panel)
+        else:
+            warn = "Invalid Username or password"
+            tkMessageBox.showerror('', warn)
+
+def chat_bot(panel):
+    panel.withdraw()
+    chatbotFrame = Toplevel()
+    chatbotFrame.title("Chatbot")
+    chatbotFrame.geometry('1000x500')
+    chatbotFrame.configure(background = '#DBD0C0')
+
+    chat_history = Text(chatbotFrame, state='disabled', wrap='word')
+    chat_history.place(x = 50, y = 80)
+    
+    user_input = Entry(chatbotFrame, width = 100)
+    user_input.place(x = 50, y = 50)
+    user_input.bind('<Return>', lambda _: send_message(user_input, chat_history))
+    
+    btn_send = Button(chatbotFrame, text = "Send", command = lambda: send_message(user_input, chat_history))
+    btn_send.place(x = 660, y = 50)
+
+    # Exit Protocol
+    chatbotFrame.wm_protocol("WM_DELETE_WINDOW", lambda: exit(chatbotFrame, panel))
+
+    # TO DO:
+        # Capture user input
+        # Call training.py and pass down the user input to the chatbot function
+        # Return respond and display in the screen
+
+def send_message(user_input, chat_history):
+    message = user_input.get()
+    if message:
+        user_input.delete(0, 'end')
+        display_message(f"You: {message} \n", chat_history)
+        # This function will not work I believe
+            # response = chat_bot_function(message.lower())
+        response = "dummy text \n"
+        display_message(response, chat_history)
+
+def display_message(message, chat_history):
+    chat_history.configure(state='normal')
+    chat_history.insert('end', message)
+    chat_history.configure(state='disabled')
+
+# Exit Toplevel Window Function
+def exit(panel_old, panel_new):
+    panel_old.withdraw()
+    panel_new.deiconify()
 
 
-def Exit():
+# Exit Program Function
+def on_closing():
     result = tkMessageBox.askquestion('System', 'Are you sure you want to exit?', icon="warning")
     if result == 'yes':
         root.destroy()
-        exit()
-
-
-def LoginForm():
-    global LoginFrame, lbl_result1
-    LoginFrame = Frame(root)
-    LoginFrame.pack(side=TOP, pady=80)
-    lbl_username = Label(LoginFrame, text="Username:", font=('arial', 25), bd=18)
-    lbl_username.grid(row=1)
-    lbl_password = Label(LoginFrame, text="Password:", font=('arial', 25), bd=18)
-    lbl_password.grid(row=2)
-    lbl_result1 = Label(LoginFrame, text="", font=('arial', 18))
-    lbl_result1.grid(row=3, columnspan=2)
-    username = Entry(LoginFrame, font=('arial', 20), textvariable=USERNAME, width=15)
-    username.grid(row=1, column=1)
-    password = Entry(LoginFrame, font=('arial', 20), textvariable=PASSWORD, width=15, show="*")
-    password.grid(row=2, column=1)
-    btn_login = Button(LoginFrame, text="Login", font=('arial', 18), width=35, command=Login)
-    btn_login.grid(row=4, columnspan=2, pady=20)
-    lbl_register = Label(LoginFrame, text="Register", fg="Blue", font=('arial', 12))
-    lbl_register.grid(row=0, sticky=W)
-    lbl_register.bind('<Button-1>', ToggleToRegister)
-
-def RegisterForm():
-    global RegisterFrame, lbl_result2
-    RegisterFrame = Frame(root)
-    RegisterFrame.pack(side=TOP, pady=40)
-    lbl_username = Label(RegisterFrame, text="Username:", font=('arial', 18), bd=18)
-    lbl_username.grid(row=1)
-    lbl_password = Label(RegisterFrame, text="Password:", font=('arial', 18), bd=18)
-    lbl_password.grid(row=2)
-    lbl_firstname = Label(RegisterFrame, text="Firstname:", font=('arial', 18), bd=18)
-    lbl_firstname.grid(row=3)
-    lbl_lastname = Label(RegisterFrame, text="Lastname:", font=('arial', 18), bd=18)
-    lbl_lastname.grid(row=4)
-    lbl_result2 = Label(RegisterFrame, text="", font=('arial', 18))
-    lbl_result2.grid(row=5, columnspan=2)
-    username = Entry(RegisterFrame, font=('arial', 20), textvariable=USERNAME, width=15)
-    username.grid(row=1, column=1)
-    password = Entry(RegisterFrame, font=('arial', 20), textvariable=PASSWORD, width=15, show="*")
-    password.grid(row=2, column=1)
-    firstname = Entry(RegisterFrame, font=('arial', 20), textvariable=FIRSTNAME, width=15)
-    firstname.grid(row=3, column=1)
-    lastname = Entry(RegisterFrame, font=('arial', 20), textvariable=LASTNAME, width=15)
-    lastname.grid(row=4, column=1)
-    btn_login = Button(RegisterFrame, text="Register", font=('arial', 18), width=35, command=Register)
-    btn_login.grid(row=6, columnspan=2, pady=20)
-    lbl_login = Label(RegisterFrame, text="Login", fg="Blue", font=('arial', 12))
-    lbl_login.grid(row=0, sticky=W)
-    lbl_login.bind('<Button-1>', ToggleToLogin)
-
-def ToggleToLogin(event=None):
-    RegisterFrame.destroy()
-    LoginForm()
-
-def ToggleToRegister(event=None):
-    LoginFrame.destroy()
-    RegisterForm()
-
-def Register():
-    Database()
-    if USERNAME.get == "" or PASSWORD.get() == "" or FIRSTNAME.get() == "" or LASTNAME.get == "":
-        lbl_result2.config(text="Please complete the required field!", fg="orange")
-    else:
-        cursor.execute("SELECT * FROM `member` WHERE `username` = ?", (USERNAME.get(),))
-        if cursor.fetchone() is not None:
-            lbl_result2.config(text="Username is already taken", fg="red")
-        else:
-            cursor.execute("INSERT INTO `member` (username, password, firstname, lastname) VALUES(?, ?, ?, ?)", (str(USERNAME.get()), str(PASSWORD.get()), str(FIRSTNAME.get()), str(LASTNAME.get())))
-            conn.commit()
-            USERNAME.set("")
-            PASSWORD.set("")
-            FIRSTNAME.set("")
-            LASTNAME.set("")
-            lbl_result2.config(text="Successfully Created!", fg="black")
-        cursor.close()
-        conn.close()
-def Login():
-    Database()
-    if USERNAME.get == "" or PASSWORD.get() == "":
-        lbl_result1.config(text="Please complete the required field!", fg="orange")
-    else:
-        cursor.execute("SELECT * FROM `member` WHERE `username` = ? and `password` = ?", (USERNAME.get(), PASSWORD.get()))
-        if cursor.fetchone() is not None:
-            lbl_result1.config(text="You Successfully Login", fg="blue")
-            LoginFrame.destroy()
-            chat_bot()
-        else:
-            lbl_result1.config(text="Invalid Username or password", fg="red")
-LoginForm()
-
-def chat_bot():
-    chatbotFrame = Frame(root)
-    # Create UI for chatbot application
-    # Capture user input
-    # Call training.py and pass down the user input to the chatbot function
-    # Return respond and display in the screen
-    
-#========================================MENUBAR WIDGETS==================================
-menubar = Menu(root)
-filemenu = Menu(menubar, tearoff=0)
-filemenu.add_command(label="Exit", command=Exit)
-menubar.add_cascade(label="File", menu=filemenu)
-root.config(menu=menubar)
-
 
 #========================================INITIALIZATION===================================
 if __name__ == '__main__':
+
+    root = Tk()
+    root.title("Chatbot System Started")
+    
+    width = 640
+    height = 480
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    x = (screen_width/2) - (width/2)
+    y = (screen_height/2) - (height/2)
+
+    root.geometry("%dx%d+%d+%d" % (width, height, x, y))
+    root.resizable(0, 0)
+    root.configure(background = '#FAEEE0')
+
+    # Menubar Widget
+    menubar = Menu(root)
+    filemenu = Menu(menubar, tearoff=0)
+    filemenu.add_command(label = "Exit", command = on_closing)
+    menubar.add_cascade(label = "File", menu = filemenu)
+    root.config(menu = menubar)
+
+    database()
+    chat_bot(root)
     root.mainloop()
    
--- training.py
+++ training.py
@@ -4,6 +4,8 @@ import json
 # allow user to match best response for chatbot
 from difflib import get_close_matches
 
+from main import display_message
+
 # Retrieve training data from "training_bank.json" file
 def load_training_bank(file_path: str) -> dict:
     with open (file_path, 'r') as file:
@@ -27,30 +29,31 @@ def get_answer_for_question(question: str, training_bank: dict) -> str | None:
             return q["answer"]
 
 # Chatbot process
-def chat_bot_function():
+def chat_bot_function(user_input):
     training_bank: dict = load_training_bank("training_bank.json")
-    
-    # Loop conversation until user prompt to exit
-    while True:
-        user_input: str = input('You: ').lower()
 
-        if user_input == "quit":
-            break
+    best_match: str | None = find_best_match(user_input, [q["question"] for q in training_bank["questions"]])
 
-        best_match: str | None = find_best_match(user_input, [q["question"] for q in training_bank["questions"]])
+    # If best response exists, reply back
+    if best_match:
+        answer: str = get_answer_for_question(best_match, training_bank)
+        return("trained", f'Bot: {answer}')
+    # Else, prompt user to suggest an answer for training the chatbot
+    else:
+        print("trained",'Bot: I don\'t know the answer. Can You teach me?')
+        new_answer: str = input('Type the answer or "skip" to skip: ')
 
-        # If best response exists, reply back
-        if best_match:
-            answer: str = get_answer_for_question(best_match, training_bank)
-            print(f'Bot: {answer}')
-        # Else, prompt user to suggest an answer for training the chatbot
-        else:
-            print('Bot: I don\'t know the answer. Can You teach me?')
-            new_answer: str = input('Type the answer or "skip" to skip: ')
+        # User can skip to avoid answering
+        if new_answer.lower() != 'skip':
+            training_bank["questions"].append({"question": user_input, "answer": new_answer})
+            save_training_bank("training_bank.json", training_bank)
+            print("Bot: Thank you! I learned a new response!")
 
-            # User can skip to avoid answering
-            if new_answer.lower() != 'skip':
-                training_bank["questions"].append({"question": user_input, "answer": new_answer})
-                save_training_bank("training_bank.json", training_bank)
-                print("Bot: Thank you! I learned a new response!")
-        print("")
+def train_chat_bot(training_bank, user_input):
+    print('Bot: I don\'t know the answer. Can You teach me?')
+    new_answer: str = input('Type the answer or "skip" to skip: ')
+
+    # User can skip to avoid answering
+    if new_answer.lower() != 'skip':
+        training_bank["questions"].append({"question": user_input, "answer": new_answer})
+        save_training_bank("training_bank.json", training_bank)
